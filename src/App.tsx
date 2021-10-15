758132cb7dfc2e078e415a6adda770e2c5e49edf
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-10-15 15:14:09 -0400

    appropriate theme...?

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {HeaderProvider} from './prodivers';
 import {Navigation} from './foundation';
+import {useColorScheme} from 'react-native';
 
 export default function App() {
+  const useDarkTheme = useColorScheme() === 'dark';
+  const theme = useDarkTheme ? DarkTheme : DefaultTheme;
+
   return (
     <PaperProvider>
       <HeaderProvider>
-        <NavigationContainer>
+        <NavigationContainer theme={theme}>
           <Navigation />
         </NavigationContainer>
       </HeaderProvider>
