20dc0985566638ff29aa60f860467a87eb388393
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-10-16 12:16:45 -0400

    more padding adjustments

--- src/components/CategoriesCollection/CategoriesCollectionSection.tsx
+++ src/components/CategoriesCollection/CategoriesCollectionSection.tsx
@@ -38,7 +38,8 @@ export default function CategoriesCollectionSection<T>({
           horizontal
           data={Array.from({length: skeletonLength})} // 5 skeleton items
           style={{marginTop: 10}}
-          contentContainerStyle={{marginHorizontal: 5}}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{marginHorizontal: 10}}
           renderItem={() => (
             <View style={{marginLeft: 10}}>
               <ThumbnailSkeleton {...dimensions} />
@@ -49,12 +50,13 @@ export default function CategoriesCollectionSection<T>({
     );
   }
   return (
-    <View style={{marginTop: 5}}>
+    <View style={{marginTop: 5, marginBottom: 15}}>
       <Title style={{marginHorizontal: 20}}>{title}</Title>
       <FlatList
         horizontal
         data={data}
         style={{marginTop: 10}}
+        showsHorizontalScrollIndicator={false}
         contentContainerStyle={{marginHorizontal: 10}}
         renderItem={({item}) => (
           <View style={{marginRight: 10}}>{renderItem(item, dimensions)}</View>
